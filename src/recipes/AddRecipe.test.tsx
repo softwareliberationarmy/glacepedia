fe00a2f0b4e3d756788b54739e130d132fd65fdb
Kerry Patrick <kpatrick@mmmicrosystems.com>
2024-04-07 18:08:27 -0500

    adding submit button enabled test and invalid ingredient test

--- src/recipes/AddRecipe.test.tsx
+++ src/recipes/AddRecipe.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import AddRecipe from "./AddRecipe";
@@ -9,19 +9,44 @@ describe("Add Recipe page", () => {
     expect(addRecipeButton()).toBeDisabled();
   });
 
-  it("disables the submit button when there are no ingredients", () => {
+  it("disables the submit button when there are no ingredients", async () => {
     render(<AddRecipe />);
-    enterRecipeNameOf("Vanilla");
+    await enterRecipeName("Vanilla");
     expect(addRecipeButton()).toBeDisabled();
   });
 
+  it("disables the submit button when I add an invalid ingredient", async () => {
+    render(<AddRecipe />);
+    await enterRecipeName("Vanilla");
+    await addIngredient("1");
+    await waitFor(() => {
+      expect(addRecipeButton()).toBeDisabled();
+    });
+    expect(screen.getByText("Invalid ingredient. Please try again.")).toBeVisible();
+  });
+
+  it("enables the submit button when I add a name and a valid ingredient and type enter", async () => {
+    render(<AddRecipe />);
+    await enterRecipeName("Vanilla");
+    const ingredient = "1c Milk";
+    await addIngredient(ingredient);
+    await waitFor(() => {
+      expect(addRecipeButton()).toBeEnabled();
+    });
+  });
 });
 
-function enterRecipeNameOf(flavorName: string) {
-    userEvent.type(screen.getByLabelText("Name"), flavorName);
+function addIngredient(ingredient: string) {
+  return userEvent.type(
+    screen.getByLabelText("Ingredients"),
+    ingredient + "{Enter}"
+  );
+}
+
+async function enterRecipeName(flavorName: string) {
+  await userEvent.type(screen.getByLabelText("Name"), flavorName);
 }
 
 function addRecipeButton(): HTMLElement {
-    return screen.getByRole("button", { name: "Add Recipe" });
+  return screen.getByRole("button", { name: "Add Recipe" });
 }
-
